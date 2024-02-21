0b9ef0b5079f6ded1522c5173537bfd9c2d11d1c
emonmahdi <developeremon1997@gmail.com>
2024-02-21 12:42:22 +0600

    add todo functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,73 @@
+import { useState } from "react";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, inputValue]);
+    setInputValue("");
+  };
+
   return (
     <>
-      <h2>React To Do</h2>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h2>React To Do</h2>
+        <div>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              placeholder="Add todo...."
+            />
+            <button type="submit">Add To Do</button>
+          </form>
+        </div>
+        <div>
+          <ul>
+            {todos.map((todo) => (
+              <li
+                key={todo}
+                style={{
+                  listStyle: "none",
+                  margin: "10px 15px",
+                  background: "black",
+                  color: "white",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  padding: "10px 20px",
+                  borderRadius: "5px",
+                }}
+              >
+                {todo}
+                <button
+                  style={{
+                    margin: "0 10px",
+                    background: "red",
+                    color: "white",
+                    padding: "10px",
+                    border: "none",
+                    borderRadius: "6px",
+                    cursor: "pointer",
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
